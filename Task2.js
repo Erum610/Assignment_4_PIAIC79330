05159821834905e04a6c5e910c567f29b55fc7d2
Erum610 <68341586+Erum610@users.noreply.github.com>
2021-01-27 00:23:30 +0500

    Delete Task2.js

--- Task2.js
+++
@@ -1,9 +0,0 @@
-var a = "472";
-document.write("Value: " + a + "<br>");
-document.write("Type: " + typeof(a) + "<br>");
-var b= parseInt(a);
-document.write("Value: " + b + "<br>");
-document.write("Type: " + typeof(b));
-
-
-
