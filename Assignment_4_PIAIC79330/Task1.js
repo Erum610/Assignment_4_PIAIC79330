2e831a6cf24dc2c990f6e28ae42dcfcd0f391160
Erum610 <68341586+Erum610@users.noreply.github.com>
2021-01-27 00:24:50 +0500

    Add files via upload

---
+++ Assignment_4_PIAIC79330/Task1.js
@@ -0,0 +1,10 @@
+var city=("Hyder"+ "abad");
+document.write("City: " + city + "<br>");
+var city1= city.replace("Hyder","Islam");
+document.write("After replacement : " + city1);
+
+
+
+
+
+
---
+++ Assignment_4_PIAIC79330/Task2.js
@@ -0,0 +1,9 @@
+var a = "472";
+document.write("Value: " + a + "<br>");
+document.write("Type: " + typeof(a) + "<br>");
+var b= parseInt(a);
+document.write("Value: " + b + "<br>");
+document.write("Type: " + typeof(b));
+
+
+
