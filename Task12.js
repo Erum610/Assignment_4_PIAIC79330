837213453d01ab313d736e59d354150ff6e7ff8b
Erum610 <68341586+Erum610@users.noreply.github.com>
2021-02-02 12:28:43 +0500

    Add files via upload

---
+++ Task12.js
@@ -0,0 +1,8 @@
+var date= prompt("Enter date");
+date=parseInt(date);
+if(date<16){
+    console.log("First fifteen days of the month")
+}
+else{
+    console.log("Last fifteen days of the month")
+}
