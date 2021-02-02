74cf87216bdbdf60132342590161bf15278ff205
Erum610 <68341586+Erum610@users.noreply.github.com>
2021-02-02 09:57:38 +0500

    Add files via upload

---
+++ Task4.js
@@ -0,0 +1,16 @@
+var cities_names= ["Karachi", "Lahore", "Islamabad", "Quetta","Peshawar"];
+document.write("Cities List: <br>"  );
+document.write(cities_names + "<br>");
+document.write("Selected cities List: <br>");
+var selected_cities;
+var a= cities_names.slice(2,4);
+document.write(a);
+
+
+
+
+
+
+
+
+
